9fb0e64bc6af1857ec078c8c75d1105daacd073d
Adrien Pensart <crunchengine@gmail.com>
2018-09-12 23:19:30 +0000

    coverage script

---
+++ coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+export MB_DATABASE=musicbot_test
+export MB_DB_PASSWORD=musicbot
+coverage run --source lib -m test.test
+coverage-badge -f -o doc/coverage.svg
+coverage report -m
