10772bf1e1a58e382da92b682039016c4fb9dc3f
Adrien Pensart <adrien.pensart@corp.ovh.com>
2018-09-25 10:45:39 -0400

    better wsgi handling

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 my_dir="$(dirname "$0")"
 export MB_DB='postgresql://postgres:musicbot@localhost:5432/musicbot_prod'
@@ -8,4 +8,8 @@ export MB_SERVER_CACHE=0
 export MB_CLIENT_CACHE=1
 export AIOCACHE_DISABLE=1
 export MB_HTTP_PASSWORD=musicbot
-$HOME/.pyenv/shims/gunicorn lib.server:app --pythonpath=$my_dir/.. --bind 127.0.0.1:1338 --worker-class sanic.worker.GunicornWorker $@
+$HOME/.pyenv/shims/gunicorn lib.web.wsgi:app \
+	--pythonpath=$my_dir/.. \
+	--bind 127.0.0.1:1338 \
+	--worker-class sanic.worker.GunicornWorker \
+	$@
