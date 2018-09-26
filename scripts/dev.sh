3d7a1430ef688db51e3420d9add14818f4cb566f
Adrien Pensart <adrien.pensart@corp.ovh.com>
2018-09-26 08:16:06 -0400

    new settings

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,6 +2,9 @@
 
 export AIOCACHE_DISABLE=1
 export MB_DB='postgresql://postgres:musicbot@localhost:5432/musicbot_prod'
+export MB_SERVER_CACHE=0
+export MB_CLIENT_CACHE=0
+export MB_WATCHER=1
 export MB_NO_AUTH=1
 export MB_DEBUG=1
 my_dir="$(dirname "$0")"
--- scripts/prod.sh
+++ scripts/prod.sh
@@ -2,9 +2,9 @@
 
 my_dir="$(dirname "$0")"
 export MB_DB='postgresql://postgres:musicbot@localhost:5432/musicbot_prod'
-export MB_WATCHER=0
+export MB_WATCHER=1
 export MB_AUTOSCAN=1
-export MB_SERVER_CACHE=0
+export MB_SERVER_CACHE=1
 export MB_CLIENT_CACHE=1
 export AIOCACHE_DISABLE=1
 export MB_HTTP_PASSWORD=musicbot
