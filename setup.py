29afff501605fa805812592c2eb570cc277e34e5
Adrien Pensart <crunchengine@gmail.com>
2020-09-21 19:36:15 +0000

    bump version to 0.7.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='musicbot',
-    version='0.7.0',
+    version='0.7.1',
     description='Music swiss army knife',
     python_requires='<3.9,>=3.8',
     author='Adrien Pensart',
