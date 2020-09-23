3d299fd44f5e8d93a4a9fab3f4831b4aa7ff3d3f
Adrien Pensart <crunchengine@gmail.com>
2020-09-23 01:52:08 +0000

    removed unused version command and update click-skeleton

--- musicbot/cli.py
+++ musicbot/cli.py
@@ -5,7 +5,7 @@ from click_skeleton import skeleton, sensible_context_settings, add_options
 from musicbot.global_options import options
 
 PROG_NAME = "musicbot"
-__version__ = "0.7.3"
+__version__ = "0.7.4"
 CONTEXT_SETTINGS = sensible_context_settings(PROG_NAME, __version__, auto_envvar_prefix='MB')
 logger = logging.getLogger(PROG_NAME)
 
@@ -20,12 +20,3 @@ def main_cli(ctx, **kwargs):
     config_obj.set(**kwargs)
     ctx.obj.config = config_obj
     logger.debug(f"Imported commands: {commands.modules}")
-
-
-@main_cli.command('version', short_help='Print version')
-def _version() -> None:
-    '''Print version
-
-       Equivalent : -V
-    '''
-    click.echo(f"{click.style(PROG_NAME, fg='yellow')}, version {click.style(__version__, fg='green')}")
