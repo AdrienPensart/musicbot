01aab0ac1a7b5c71f3eb5e0a814828606cc0ae3a
Adrien Pensart <crunchengine@gmail.com>
2020-10-03 03:03:46 +0000

    spotify test diff missing common args

--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -8,14 +8,21 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.runner_setup(mix_stderr=False)
 def test_spotify_playlists(cli_runner):
-    run_cli(cli_runner, main_cli, ['spotify', 'playlists'])
+    run_cli(cli_runner, main_cli, [
+        'spotify', 'playlists'
+    ])
 
 
 @pytest.mark.runner_setup(mix_stderr=False)
 def test_spotify_tracks(cli_runner):
-    run_cli(cli_runner, main_cli, ['spotify', 'tracks'])
+    run_cli(cli_runner, main_cli, [
+        'spotify', 'tracks'
+    ])
 
 
 @pytest.mark.runner_setup(mix_stderr=False)
-def test_spotify_diff(cli_runner):
-    run_cli(cli_runner, main_cli, ['spotify', 'diff'])
+def test_spotify_diff(cli_runner, common_args):
+    run_cli(cli_runner, main_cli, [
+        'spotify', 'diff',
+        *common_args,
+    ])
