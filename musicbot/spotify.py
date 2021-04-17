f23b945267c0a23e7974d4113b0d52f9b7a04b58
Adrien Pensart <adrien.pensart@corp.ovh.com>
2021-04-17 12:28:25 +0000

    fixed get cached token warning

--- musicbot/spotify.py
+++ musicbot/spotify.py
@@ -19,66 +19,70 @@ class Spotify:
     redirect_uri: str
     token: str
 
-    @property
-    def _auth_manager(self) -> spotipy.oauth2.SpotifyOAuth:
+    @functools.cached_property
+    def auth_manager(self) -> spotipy.oauth2.SpotifyOAuth:
         auth_params = attr.asdict(self)
         del auth_params['token']
         del auth_params['cache_path']
         del auth_params['username']
-        cache_handler = CacheFileHandler(cache_path=self.cache_path, username=self.username)
-        return spotipy.oauth2.SpotifyOAuth(**auth_params, cache_handler=cache_handler)
+        return spotipy.oauth2.SpotifyOAuth(**auth_params, cache_handler=self.cache_handler)
 
     @functools.cached_property
-    def _api(self) -> spotipy.Spotify:
+    def cache_handler(self):
+        return CacheFileHandler(cache_path=self.cache_path, username=self.username)
+
+    @functools.cached_property
+    def api(self) -> spotipy.Spotify:
         if self.token:
             return spotipy.Spotify(auth=self.token)
-        return spotipy.Spotify(auth_manager=self._auth_manager)
+        return spotipy.Spotify(auth_manager=self.auth_manager)
 
     def new_token(self) -> Any:
-        return self._auth_manager.get_access_token(check_cache=False)
+        return self.auth_manager.get_access_token(check_cache=False)
 
     def cached_token(self) -> Any:
-        token = self._auth_manager.get_access_token()
+        token = self.cache_handler.get_cached_token()
         if not token:
             logger.warning("no cached token")
+        self.auth_manager.validate_token(token)
         return token
 
     def is_token_expired(self) -> bool:
         token = self.cached_token()
         if not token:
             return True
-        return self._auth_manager.is_token_expired(token)
+        return self.auth_manager.is_token_expired(token)
 
     def refresh_token(self) -> Any:
         token = self.cached_token()
         if not token:
             return None
-        return self._auth_manager.refresh_access_token(token['refresh_token'])
+        return self.auth_manager.refresh_access_token(token['refresh_token'])
 
     def get_download_playlist(self):
         name = "To Download"
         download_playlist = self.playlist(name)
         if download_playlist:
             return download_playlist
-        return self._api.user_playlist_create(self.username, name, public=False)
+        return self.api.user_playlist_create(self.username, name, public=False)
 
     def set_download_playlist(self, tracks):
         download_playlist = self.get_download_playlist()
         track_ids = [track['track']['id'] for track in tracks]
 
         # erase playlist first
-        self._api.user_playlist_replace_tracks(self.username, download_playlist['id'], [])
+        self.api.user_playlist_replace_tracks(self.username, download_playlist['id'], [])
 
         # add tracks 100 by 100 (API limit)
         for i in range(0, len(track_ids), 100):
-            self._api.user_playlist_add_tracks(self.username, download_playlist['id'], track_ids[i:i + 100])
+            self.api.user_playlist_add_tracks(self.username, download_playlist['id'], track_ids[i:i + 100])
 
     def tracks(self) -> List[Any]:
         offset = 0
         limit = 50
         objects = []
         while True:
-            new_objects = self._api.current_user_saved_tracks(limit=limit, offset=offset)
+            new_objects = self.api.current_user_saved_tracks(limit=limit, offset=offset)
             objects.append(new_objects['items'])
             length = len(new_objects['items'])
             logger.info(f"chunk size: {length}")
@@ -92,7 +96,7 @@ class Spotify:
         limit = 50
         objects = []
         while True:
-            new_objects = self._api.current_user_playlists(limit=limit, offset=offset)
+            new_objects = self.api.current_user_playlists(limit=limit, offset=offset)
             length = len(new_objects['items'])
             objects.append(new_objects['items'])
             logger.info(f"chunk size: {length}")
@@ -114,11 +118,11 @@ class Spotify:
         for p in playlists:
             if p['name'] == name:
                 tracks = []
-                results = self._api.playlist(p['id'], fields="tracks,next")
+                results = self.api.playlist(p['id'], fields="tracks,next")
                 new_tracks = results['tracks']
                 tracks.append(new_tracks['items'])
                 while new_tracks['next']:
-                    new_tracks = self._api.next(new_tracks)
+                    new_tracks = self.api.next(new_tracks)
                     tracks.append(new_tracks['tracks']['items'])
                 return list(itertools.chain(*tracks))
         return []
