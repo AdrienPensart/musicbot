43495b5eee6becedd6158b821942cdf6a705f7e9
Adrien Pensart <adrien.pensart@corp.ovh.com>
2023-08-31 11:31:59 -0400

    skip tests if docker service not available

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from beartype import beartype
 from click.testing import CliRunner
-from pytest import fixture
+from pytest import fixture, skip
 
 from musicbot.file import File
 from musicbot.folders import Folders
@@ -25,17 +25,16 @@ def cli_runner() -> CliRunner:
 
 
 @beartype
-def wait_for_service(hostname: str, port: int, timeout: int = 60) -> None:
+def wait_for_service(hostname: str, port: int, timeout: int = 60) -> bool:
     start_time = time.perf_counter()
     while True:
         try:
             with socket.create_connection((hostname, port), timeout=timeout):
-                break
-        except OSError as e:
+                return True
+        except OSError:
             time.sleep(0.01)
             if time.perf_counter() - start_time >= timeout:
-                msg = f"Waited too long for {hostname}:{port} to start accepting connections."
-                raise TimeoutError(msg) from e
+                return False
 
 
 @fixture(scope="session")
@@ -44,7 +43,8 @@ def edgedb() -> str:
     hostname = "127.0.0.1"
     port = 15656
     dsn = f"""edgedb://edgedb:musicbot@{hostname}:{port}"""
-    wait_for_service(hostname, port)
+    if not wait_for_service(hostname, port):
+        _ = skip(f"Timeout during wait for {dsn}")
     return dsn
 
 
